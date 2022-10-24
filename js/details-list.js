bc061967535f6a4ad923f78895d682a749d923a5
Sergey <79069595+yunsergey1@users.noreply.github.com>
2022-10-24 17:01:58 +0800

    Add files via upload

---
+++ js/details-list.js
@@ -0,0 +1,59 @@
+const detailsList = [
+    {
+        img: '/img/six-01.jpg',
+        title: 'Инфраструктура микрогорода',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 350,
+    },
+    {
+        img: '/img/six-02.jpg',
+        title: 'Видовые квартиры',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 286,
+    },
+    {
+        img: '/img/six-03.jpg',
+        title: 'Свой парк и набережная',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 415,
+    },
+    {
+        img: '/img/six-04.jpg',
+        title: 'Витражное остекление лоджий',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 415,
+    },
+    {
+        img: '/img/six-05.jpg',
+        title: 'Фасады, облицованные гранитом',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 350,
+    },
+    {
+        img: '/img/six-06.jpg',
+        title: 'Кладовые',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 286,
+    },
+    {
+        img: '/img/six-07.jpg',
+        title: 'Горячая вода 24/7',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 350,
+    },
+    {
+        img: '/img/six-08.jpg',
+        title: 'Колясочные',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 286,
+    },
+    {
+        img: '/img/six-09.jpg',
+        title: 'Места для ожидания в подъездах',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis excepturi, voluptatum quasi veniam delectus hic voluptas corrupti.',
+        width: 415,
+    },
+
+]
+
+export default detailsList;
---
+++ js/plan-list.js
@@ -0,0 +1,16 @@
+const planList = [
+    {
+        img: '/img/five-card-01.jpg',
+        title: 'Эффективные планировки',
+    },
+    {
+        img: '/img/five-card-02.jpg',
+        title: 'Высокие потолки',
+    },
+    {
+        img: '/img/five-card-03.jpg',
+        title: 'Двухкамерные стеклопакеты',
+    },
+]
+
+export default planList;
